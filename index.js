5f320cbb20329d7691df9d2a5ef6dd2ea4b72b36
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-22 09:58:33 +0200

    make buttons functionalty

--- index.js
+++ index.js
@@ -16,25 +16,45 @@
 // }
 
 //toggle key////////////////////////////////////////////////////////
+var submitBtn = document.getElementById("btn");
+submitBtn.addEventListener("click", () => {
+  var elements = document.getElementById("myForm").elements;
+  var obj = {};
+  for (var i = 0; i < elements.length; i++) {
+    var item = elements.item(i);
+    obj[item.name] = item.value;
+  }
+  if (Object.keys(obj).length >= 8) {
+    console.log(obj);
+    alert("placing order");
+    palceInitialOrder(obj);
+  }
+});
+
+document.getElementById("loadBtn").addEventListener("click", () => {
+  loadData();
+});
+document.getElementById("saveBtn").addEventListener("click", () => {
+  saveData();
+});
 let statusChecker;
 var socketData = runWebSocket();
 document.querySelector("input[type=checkbox]").onchange = (e) => {
-    if (e.target.checked) {
-        disableInputs();
-        var elements = document.getElementById("myForm").elements;
-        var obj = {};
-        for (var i = 0; i < elements.length; i++) {
-            var item = elements.item(i);
-            obj[item.name] = item.value;
-        }
-        palceInitialOrder(obj);
-        statusChecker = setInterval(checkSatatus(obj, socketData), 1 * 1000);
-    } else {
-        clearInterval(statusChecker)
-        enableInputs();
+  if (e.target.checked) {
+    disableInputs();
+    var elements = document.getElementById("myForm").elements;
+    var obj = {};
+    for (var i = 0; i < elements.length; i++) {
+      var item = elements.item(i);
+      obj[item.name] = item.value;
     }
-
-}
+    palceInitialOrder(obj);
+    statusChecker = setInterval(checkSatatus(obj, socketData), 1 * 1000);
+  } else {
+    clearInterval(statusChecker);
+    enableInputs();
+  }
+};
 
 // //this is for adding the input values in the local storage and the other for retriving it from the localstorge
 // document.getElementById("saveBtn").onclick = () => {
@@ -45,8 +65,6 @@ document.querySelector("input[type=checkbox]").onchange = (e) => {
 //     loadDate();
 //     alert('your last values have been retrived');
 // }
-
-
 
 // module.exports 1= runWebSocket;
 // const WebSocket = require('ws');
@@ -60,7 +78,3 @@ document.querySelector("input[type=checkbox]").onchange = (e) => {
 // ws.on('message', function incoming(data) {
 //     console.log(data);
 // });
-
-
-
-
